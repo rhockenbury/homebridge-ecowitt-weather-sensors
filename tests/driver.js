8ab99733786d40d0065abfea8713c38fb21904e6
Ryler Hockenbury <ryler.hockenbury@gmail.com>
2024-08-12 15:15:10 -0400

    Introduce tests scaffolding

---
+++ tests/driver.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const hap = require("hap-nodejs");
+const platform = require('./../dist/EcowittPlatform');
+const platformConfig = require('./configs/default.json')
+
+// mock api object
+const api = {
+  "hap": hap,
+  "user": { "storagePath": function noop(){} },
+  "on": function noop(){},
+  "platformAccessory": hap.Accessory,
+  "registerPlatformAccessories": function noop(){}
+};
+
+module.exports = function() {
+  return new platform.EcowittPlatform(console, platformConfig, api);
+}
