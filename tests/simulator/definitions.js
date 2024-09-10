6da71131c30a4acb71bea2453e658452589979e4
Ryler Hockenbury <ryler.hockenbury@gmail.com>
2024-09-10 11:57:21 -0400

    Add initial data report simulator

---
+++ tests/simulator/definitions.js
@@ -0,0 +1,15 @@
+
+export const definitions = {
+  "tempinf": {
+    "min": 20,
+    "max": 100,
+    "minIncrement": 1,
+    "maxIncrement": 5
+  },
+  "humidityin": {
+    "min": 0,
+    "max": 100,
+    "minIncrement": 0,
+    "maxIncrement": 5
+  }
+}
