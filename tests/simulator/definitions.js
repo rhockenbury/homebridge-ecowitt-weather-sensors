840c9b3a9a68420f5fa246a32f2a375f65d28855
Ryler Hockenbury <ryler.hockenbury@gmail.com>
2024-09-18 18:26:36 -0400

    Prep v2.4.1 GA release

--- tests/simulator/definitions.js
+++ tests/simulator/definitions.js
@@ -1,15 +1,195 @@
 
-export const definitions = {
-  "tempinf": {
+module.exports = {
+  "temp": {
     "min": 20,
     "max": 100,
     "minIncrement": 1,
     "maxIncrement": 5
   },
-  "humidityin": {
+  "humidity": {
     "min": 0,
     "max": 100,
     "minIncrement": 0,
     "maxIncrement": 5
+  },
+  "winddir": {
+    "min": 0,
+    "max": 360,
+    "minIncrement": 0,
+    "maxIncrement": 2
+  },
+  "windspeedmph": {
+    "min": 0,
+    "max": 50,
+    "minIncrement": 0,
+    "maxIncrement": 2
+  },
+  "windgustmph": {
+    "min": 0,
+    "max": 50,
+    "minIncrement": 0,
+    "maxIncrement": 2
+  },
+  "maxdailygust": {
+    "min": 0,
+    "max": 50,
+    "minIncrement": 0,
+    "maxIncrement": 2
+  },
+  "solarradiation": {
+    "min": 0,
+    "max": 700,
+    "minIncrement": 1,
+    "maxIncrement": 50
+  },
+  "uv": {
+    "min": 0,
+    "max": 1,
+    "minIncrement": 0,
+    "maxIncrement": 1
+  },
+  "rainratein": {
+    "min": 0,
+    "max": 4,
+    "minIncrement": 0,
+    "maxIncrement": 0.25
+  },
+  "eventrainin": {
+    "min": 0,
+    "max": 4,
+    "minIncrement": 0,
+    "maxIncrement": 0.25
+  },
+  "hourlyrainin": {
+    "min": 0,
+    "max": 4,
+    "minIncrement": 0,
+    "maxIncrement": 0.25
+  },
+  "dailyrainin": {
+    "min": 0,
+    "max": 10,
+    "minIncrement": 0,
+    "maxIncrement": 0.5
+  },
+  "weeklyrainin": {
+    "min": 0,
+    "max": 10,
+    "minIncrement": 0,
+    "maxIncrement": 0.5
+  },
+  "monthlyrainin": {
+    "min": 0,
+    "max": 10,
+    "minIncrement": 0,
+    "maxIncrement": 0.5
+  },
+  "yearlyrainin": {
+    "min": 0,
+    "max": 30,
+    "minIncrement": 0,
+    "maxIncrement": 1
+  },
+  "rrain_piezo": {
+    "min": 0,
+    "max": 4,
+    "minIncrement": 0,
+    "maxIncrement": 0.25
+  },
+  "erain_piezo": {
+    "min": 0,
+    "max": 4,
+    "minIncrement": 0,
+    "maxIncrement": 0.25
+  },
+  "hrain_piezo": {
+    "min": 0,
+    "max": 4,
+    "minIncrement": 0,
+    "maxIncrement": 0.25
+  },
+  "drain_piezo": {
+    "min": 0,
+    "max": 10,
+    "minIncrement": 0,
+    "maxIncrement": 0.5
+  },
+  "wrain_piezo": {
+    "min": 0,
+    "max": 10,
+    "minIncrement": 0,
+    "maxIncrement": 0.5
+  },
+  "mrain_piezo": {
+    "min": 0,
+    "max": 30,
+    "minIncrement": 0,
+    "maxIncrement": 1
+  },
+  "yrain_piezo": {
+    "min": 0,
+    "max": 30,
+    "minIncrement": 0,
+    "maxIncrement": 1
+  },
+  "soilmoisture": {
+    "min": 0,
+    "max": 100,
+    "minIncrement": 0,
+    "maxIncrement": 3
+  },
+  "soilad": {
+    "min": 70,
+    "max": 500,
+    "minIncrement": 0,
+    "maxIncrement": 5
+  },
+  "pm25": {
+    "min": 0,
+    "max": 75,
+    "minIncrement": 0,
+    "maxIncrement": 2
+  },
+  "pm10": {
+    "min": 0,
+    "max": 120,
+    "minIncrement": 0,
+    "maxIncrement": 3
+  },
+  "tf": {
+    "min": 20,
+    "max": 100,
+    "minIncrement": 1,
+    "maxIncrement": 5
+  },
+  "humi": {
+    "min": 0,
+    "max": 100,
+    "minIncrement": 0,
+    "maxIncrement": 5
+  },
+  "lightning_num": {
+    "min": 0,
+    "max": 5,
+    "minIncrement": 1,
+    "maxIncrement": 1
+  },
+  "lightning_time": {
+    "min": 300,
+    "max": 604800000 ,
+    "minIncrement": 300,
+    "maxIncrement": 50000
+  },
+  "leak": {
+    "min": 0,
+    "max": 1,
+    "minIncrement": 1,
+    "maxIncrement": 1
+  },
+  "leafwetness": {
+    "min": 0,
+    "max": 100,
+    "minIncrement": 0,
+    "maxIncrement": 3
   }
 }
